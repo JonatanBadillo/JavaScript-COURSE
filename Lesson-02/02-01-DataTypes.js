192802ce44a3f236d7697d1bfc5a0eb420f12bed
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-25 17:45:37 -0600

    Data Types

---
+++ Lesson-02/02-01-DataTypes.js
@@ -0,0 +1,14 @@
+// Data Types
+var name = "Jonatan"; // String
+console.log(name);
+
+var number = 1000; // Numeric type
+console.log(number); 
+
+var object = { // Object Type
+    name : "Enrique",
+    lastName : "Badillo",
+    telephone : "55443322"
+} 
+
+console.log(object);
