35318608f7bda3f38ae87b2a42f88b30f4a62946
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-25 17:58:33 -0600

    Data Types 2

--- Lesson-02/02-01-DataTypes.js
+++ Lesson-02/02-01-DataTypes.js
@@ -17,3 +17,33 @@ var object = { // Object Type
 
 console.log(object);
 console.log(typeof object)
+
+var flag = true; // Boolean Type
+console.log(flag);
+console.log(typeof flag);
+
+function myFunction(){ }// Function type
+console.log(myFunction);
+console.log(typeof myFunction)
+
+var symbol = Symbol("my symbol") // Symbol Type
+console.log(symbol);
+console.log(typeof symbol);
+
+class Person{ // Class type is a function
+    constructor(name, lastname){
+        this.name = name;
+        this.lastname = lastname;
+    }
+}
+console.log(Person)
+console.log(typeof Person);
+
+// Undefined Type
+var x;
+console.log(typeof x);
+
+// null = no value
+var y = null;
+console.log(y);
+console.log(typeof y)
