51394822946c3871cc24a1179fcd0061250ea6e9
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-26 17:18:02 -0600

    Arithmetic Operators

---
+++ Lesson-03/03-01-Operators.js
@@ -0,0 +1,19 @@
+let a = 3, b = 2;
+let z = a + b;
+console.log("The result of the addition is: " + z);
+
+z = a - b;
+console.log("The result of the substraction is: " + z);
+
+z = a * b;
+console.log("The result of the multiplication is: " + z);
+
+z = a / b;
+console.log("The result of the division is: " + z);
+
+z = a % b;
+console.log("The residue is: " + z);
+
+z = a ** b;
+console.log("The result with the exponent is: " + z);
+
