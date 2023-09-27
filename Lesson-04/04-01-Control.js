cc63b393237e4740a5eb2c6585f4ba7df5dbf470
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-27 07:16:52 -0600

    else if

--- Lesson-04/04-01-Control.js
+++ Lesson-04/04-01-Control.js
@@ -5,3 +5,14 @@ if(condition){
 }else{
     console.log('False Condition');
 }
+
+let  number = 3;
+if(number == 1){
+    console.log('Number One');
+}else if(number == 2){
+    console.log('Number Two');
+}else if(number == 3){
+    console.log('Number Three');
+}else{
+    console.log('Unknow number');
+}
