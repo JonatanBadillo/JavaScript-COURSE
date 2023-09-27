adf8f06a16b8400e68e4f7d3ee78de13ed17c9b1
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-27 17:24:18 -0600

    return

--- Lesson-07/07-01-Functions.js
+++ Lesson-07/07-01-Functions.js
@@ -1,5 +1,7 @@
 // Functions
 console.log('Functions');
+// Hoisting
+myFunction(10,2);
 // Function Declaration
 function myFunction(a, b){
     console.log(a + ' + ' + b + ' = ' + (a+b));
@@ -7,3 +9,11 @@ function myFunction(a, b){
 
 // Calling the function
 myFunction(3,2);
+
+
+function newFunction(a,b){
+    return a + b;
+}
+
+result = newFunction(5,4);
+console.log(result);
