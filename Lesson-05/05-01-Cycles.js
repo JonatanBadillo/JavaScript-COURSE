8c6470aea37700501e3469be84ea0fafbfc7e235
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-27 08:07:03 -0600

    for

--- Lesson-05/05-01-Cycles.js
+++ Lesson-05/05-01-Cycles.js
@@ -13,3 +13,12 @@ do{
     console.log(counter2+1);
     counter2++;
 }while(counter2<3);
+
+
+// FOR
+console.log('FOR');
+
+for(let i = 0; i<5 ; i++){
+    console.log(i+1);
+}
+console.log('End of FOR CYCLE');
