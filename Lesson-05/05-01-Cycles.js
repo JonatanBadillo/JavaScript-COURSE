3bd408f10db8da3b4d278fa73d49127600a5df53
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-27 08:01:33 -0600

    do while

--- Lesson-05/05-01-Cycles.js
+++ Lesson-05/05-01-Cycles.js
@@ -5,3 +5,11 @@ while(counter< 3){
     counter++;
     console.log(counter)
 }
+
+// DO WHILE
+console.log('DO WHILE');
+let counter2 = 0;
+do{
+    console.log(counter2+1);
+    counter2++;
+}while(counter2<3);
