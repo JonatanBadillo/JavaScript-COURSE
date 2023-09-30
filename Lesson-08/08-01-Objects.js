713162be2d5c846ee8b2cbc05dc7435adb603ace
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-09-30 11:43:29 -0600

    Objects

---
+++ Lesson-08/08-01-Objects.js
@@ -0,0 +1,12 @@
+let x = 10;
+console.log(x.length);
+
+let person = {
+    name: 'Jony',
+    lastname: 'Badillo',
+    email: 'jonatanbadillo@mail.com',
+    age : 21
+}
+
+person.age = 20
+console.log(person)
