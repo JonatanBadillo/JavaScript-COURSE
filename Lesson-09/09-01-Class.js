bdf2412c54aca27b6d251c694692d70a93192091
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-10-03 12:45:53 -0600

    GET & SET

--- Lesson-09/09-01-Class.js
+++ Lesson-09/09-01-Class.js
@@ -1,7 +1,23 @@
 class Person{
     constructor(name , lastname){
-        this.name = name;
-        this.lastname = lastname;
+        this._name = name;
+        this._lastname = lastname;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+
+    get lastname(){
+        return this._lastname
+    }
+
+    set lastname(lastname){
+        this._lastname = lastname;
     }
 }
 
@@ -11,6 +27,8 @@ let person1 = new Person('Jony','Badillo');
 //person1.name = 'Jony';
 //person1.lastname = 'Badillo';
 console.log(person1);
+person1.name = 'Jonatan Enrique'; // set name('Jonatan Enrique')
+console.log(person1.name);// get name()
 
 let person2 = new Person('Memo','Ochoa');
 console.log(person2);
