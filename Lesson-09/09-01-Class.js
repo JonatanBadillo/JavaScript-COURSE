512f9e579f704d39eae100076b5e779208612864
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-10-03 12:40:34 -0600

    Classes in JS

---
+++ Lesson-09/09-01-Class.js
@@ -0,0 +1,16 @@
+class Person{
+    constructor(name , lastname){
+        this.name = name;
+        this.lastname = lastname;
+    }
+}
+
+
+// Creating the object
+let person1 = new Person('Jony','Badillo');
+//person1.name = 'Jony';
+//person1.lastname = 'Badillo';
+console.log(person1);
+
+let person2 = new Person('Memo','Ochoa');
+console.log(person2);
