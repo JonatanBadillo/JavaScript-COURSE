5ec11025760870f82caf1e1aee36832178db4dac
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-11-03 14:03:09 -0600

    client class corrected

--- Lesson-09/Client.js
+++ Lesson-09/Client.js
@@ -1,20 +1,20 @@
-class Client extends Person{
+class Client extends Person {
     static counterClient = 0;
-    constructor(name, lastname, age, registerDate){
-        super(name,lastname,age);
-        idClient = ++counterClient;
-        this.registerDate = registerDate;
+    constructor(name, lastname, age, registerDate) {
+        super(name, lastname, age);
+        this._idClient = ++Client.counterClient;
+        this._registerDate = registerDate;
     }
 
-    get registerDate(){
-        return this.registerDate;
+    get registerDate() {
+        return this._registerDate;
     }
 
-    set registerDate(registerDate){
-        this.registerDate = registerDate;
+    set registerDate(registerDate) {
+        this._registerDate = registerDate;
     }
 
-    toString(){
-        return super.toString() + ' - ' + this.idClient + ' ,' + this.registerDate;
+    toString() {
+        return super.toString() + ' - ' + this._idClient + ' ,' + this._registerDate;
     }
 }
--- Lesson-09/Proof.js
+++ Lesson-09/Proof.js
@@ -69,26 +69,27 @@ class Employee extends Person {
 }
 
 
-class Client extends Person{
+class Client extends Person {
     static counterClient = 0;
-    constructor(name, lastname, age, registerDate){
-        super(name,lastname,age);
-        idClient = ++counterClient;
-        this.registerDate = registerDate;
+    constructor(name, lastname, age, registerDate) {
+        super(name, lastname, age);
+        this._idClient = ++Client.counterClient;
+        this._registerDate = registerDate;
     }
 
-    get registerDate(){
-        return this.registerDate;
+    get registerDate() {
+        return this._registerDate;
     }
 
-    set registerDate(registerDate){
-        this.registerDate = registerDate;
+    set registerDate(registerDate) {
+        this._registerDate = registerDate;
     }
 
-    toString(){
-        return super.toString() + ' - ' + this.idClient + ' ,' + this.registerDate;
+    toString() {
+        return super.toString() + ' - ' + this._idClient + ' ,' + this._registerDate;
     }
 }
+
 
 let person1 = new Person('Jony','Badillo',21);
 console.log(person1.toString());
