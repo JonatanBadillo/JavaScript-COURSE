c0d87c9983f5b8fab660aa564031c87d52ffea87
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-11-03 13:34:56 -0600

    exercise Persons, creating class Person

---
+++ Lesson-09/Person.js
@@ -0,0 +1,45 @@
+// Creating Class Person
+class Person{
+    // creating the static attribute
+    static counterPersons = 0;
+    // creating constructor
+    constructor(name, lastname,age,idPerson){
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.idPerson = idPerson
+    }
+
+    // GET AND SET
+    get idPerson(){
+        return this.idPerson;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+
+    get lastname(){
+        return this._lastname
+    }
+
+    set lastname(lastname){
+        this._lastname = lastname;
+    }
+
+    get age(){
+        return this.age;
+    }
+
+    set age(age){
+        this.age = age;
+    }
+
+    toString(){
+        return this.idPerson + ' - ' + this._name + ' ' + this._lastname + ', ' + this.age + ' years old';
+    }
+}
