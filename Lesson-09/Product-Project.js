5b04d2a20fb864c243d6f878dbb804fa6fd9c558
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-11-04 18:44:18 -0600

    crating constructors and getters and setters of Product

---
+++ Lesson-09/Product-Project.js
@@ -0,0 +1,28 @@
+class Product{
+    static counterProducts = 0;
+    constructor(name,price){
+        this._name = name;
+        this._price = price;
+        this._idProduct = ++counterProducts;
+    }
+
+    get _idProduct() {
+        return this._idProduct;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    get price() {
+        return this._price;
+    }
+
+    set price(price){
+        this._price = price;
+    }
+}
