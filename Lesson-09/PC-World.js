42ff9b7792abb4fa1a776e8709fbafb3807c30f4
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-11-08 13:51:09 -0600

    getter & setters MONITOR

--- Lesson-09/PC-World.js
+++ Lesson-09/PC-World.js
@@ -60,6 +60,26 @@ class Monitor{
         this._brand = brand;
         this._size = size;
     }
+
+    get idMonitor(){
+        return this._idMonitor;
+    }
+
+    get brand(){
+        return this._brand;
+    }
+
+    set brand(brand){
+        this._brand = brand;
+    }
+
+    get size(){
+        return this._size;
+    }
+
+    set size(size){
+        this._size = size;
+    }
 }
 
 let m1 = new Mouse('usb','hp');
