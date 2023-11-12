caa493749878dc5c8e3a8941ffb8d1551673d281
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-11-12 07:53:19 -0600

    creating my function

--- Lesson-12/12-01-ErrorsHandling.js
+++ Lesson-12/12-01-ErrorsHandling.js
@@ -1,10 +1,12 @@
 'use strict'
 
 try{
-    x = 10;
+    let x = 10;
+    throw 'My error';
 }catch(error){
     console.log(error);
 }finally{
     console.log('Finishing the bug check');
 }
 console.log('error');
+
