43d02d3f863c5a6c277aa53b61f07f76d4102c11
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-11-14 09:31:41 -0600

    setTimeOut and Promises

--- Lesson-15/15-01-Promises.js
+++ Lesson-15/15-01-Promises.js
@@ -12,4 +12,12 @@ let myPromise = new Promise((resolve, rejected) => {
 //    error => console.log(error)
 //);
 
+
+// Other manner of applying promises
 myPromise.then(value => console.log(value)).catch(error => console.log(error));
+
+let promise = new Promise((resolved) =>{
+    setTimeout( ()=>{ resolved('Hello with Promise and TimeOut',3000)});
+});
+
+promise.then( value=> console.log(value));
