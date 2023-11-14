31c6ea3e297e35daba6cef64d592528a925d0697
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-11-14 09:34:26 -0600

    msgs added at the begining at the promise

--- Lesson-15/15-01-Promises.js
+++ Lesson-15/15-01-Promises.js
@@ -17,7 +17,9 @@ let myPromise = new Promise((resolve, rejected) => {
 myPromise.then(value => console.log(value)).catch(error => console.log(error));
 
 let promise = new Promise((resolved) =>{
+    console.log('Starting Promise')
     setTimeout( ()=>{ resolved('Hello with Promise and TimeOut',3000)});
+    console.log('End Promise')
 });
 
 promise.then( value=> console.log(value));
